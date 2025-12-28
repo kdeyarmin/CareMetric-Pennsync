594262c96c4fc05d6e782d4d948b4da38abf361d
Kevin Deyarmin <kdeyarmin@comcast.net>
2025-12-28 13:13:43 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -211,6 +211,19 @@ export default function Layout({ children, currentPageName }) {
                   {item.name}
                 </Link>
               ))}
+              <div className="border-t pt-2 mt-2">
+                {bottomNavItems.map(item => (
+                  <Link
+                    key={item.page}
+                    to={createPageUrl(item.page)}
+                    onClick={() => setMobileMenuOpen(false)}
+                    className="flex items-center gap-3 px-3 py-2 rounded hover:bg-gray-100"
+                  >
+                    <item.icon className="w-5 h-5" />
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
             </nav>
           </div>
         </div>
