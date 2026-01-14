83f16d0043335c8397a63a52169dfc5008d9ed37
Kevin Deyarmin <kdeyarmin@comcast.net>
2026-01-14 02:26:37 +0000

    File changes

--- src/pages/SmartNoteAssistant.jsx
+++ src/pages/SmartNoteAssistant.jsx
@@ -174,7 +174,7 @@ export default function SmartNoteAssistant() {
   );
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50 p-6">
+    <div className="min-h-screen p-6">
       <div className="max-w-6xl mx-auto space-y-8">
         {/* Header */}
         <div className="space-y-2">
