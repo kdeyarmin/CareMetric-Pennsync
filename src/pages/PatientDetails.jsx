ba03ab71b99cd70e16e0d1a88481206955aa151b
Kevin Deyarmin <kdeyarmin@comcast.net>
2026-01-15 05:20:43 +0000

    File changes

--- src/pages/PatientDetails.jsx
+++ src/pages/PatientDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { secureEntity } from "../components/security/SecureEntityWrapper";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
