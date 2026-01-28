cbbac6959ab5719577c098d24bb9c7263ba34aad
Kevin Deyarmin <kdeyarmin@comcast.net>
2026-01-28 01:12:03 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { base44 } from "@/api/base44Client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import { createPageUrl } from "./utils";
+import { createPageUrl } from "../utils";
 import {
   Users,
   Calendar,
